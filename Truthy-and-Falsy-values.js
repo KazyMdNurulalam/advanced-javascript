a37894f52babe13a3238254a88a219f0fea3590f
KazyMdNurulalam <kazymdnurulalam@gmail.com>
2021-10-24 22:35:11 +0600

    truthy and falsy

---
+++ Truthy-and-Falsy-values.js
@@ -0,0 +1,25 @@
+// truthy:
+// 1, -1
+// '123', "0", " ", "name"
+// true
+// []
+// {}
+
+
+// falsy:
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+
+const age = 11;
+if (age) {
+    console.log("condition is true");
+}
+
+else{
+    console.log("condition is false");
+}
