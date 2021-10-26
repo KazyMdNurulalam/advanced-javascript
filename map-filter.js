cf1b819f4c15a01f4465304c123caa921dd71600
KazyMdNurulalam <kazymdnurulalam@gmail.com>
2021-10-27 01:51:29 +0600

    map filter array object

--- map-filter.js
+++ map-filter.js
@@ -1,4 +1,4 @@
-const number = [2, 4, 6, 15, 22, 21];
+// const number = [2, 4, 6, 15, 22, 21];
 
 // by for loop
 // const result = [];
@@ -24,10 +24,36 @@ const number = [2, 4, 6, 15, 22, 21];
 
 // const result = number.map(square);
 
+const number = [2, 4, 6, 15, 22, 21];
+
 const result = number.map(x => x * x);
 console.log(result);
 
-        // filter
+const student = [
+    { roll: 01, name: "tanvir", gender: "male" },
+    { roll: 02, name: "galib", gender: "male" },
+    { roll: 03, name: "daud", gender: "male" },
+    { roll: 04, name: "ashraf", gender: "male" },
+    { roll: 05, name: "sonia", gender: "female" },
+];
+// by loop
+// const studentsName =[];
+// for (let i = 0; i < student.length; i++) {
+//     const students = student[i];
+//     const name= students.name
+//     studentsName.push(name);
+//     console.log('students ',name);    
+// };
+// console.log('studentsName ',studentsName);
+
+// by map
+const studentsName = student.map(s => s.name);
+console.log(studentsName);
+
+
+
+// filter
+// const number = [2, 4, 6, 15, 22, 21];
 const evenNum = number.filter(x => {
     if (x % 2 === 0) {
         return x;
@@ -35,7 +61,25 @@ const evenNum = number.filter(x => {
 });
 console.log(evenNum);
 
-        // Find
+// const student = [
+//     {roll: 01, name: "tanvir", gender: "male"},
+//     {roll: 02, name: "galib", gender: "male"},
+//     {roll: 03, name: "daud", gender: "male"},
+//     {roll: 04, name: "ashraf", gender: "male"},
+//     {roll: 05, name: "sonia", gender: "female"},
+// ];
+
+const badStudents = student.filter(s => {
+    if (s.roll > 3) {
+        return s;
+    }
+});
+console.log(badStudents);
+
+
+
+// Find
+// const number = [2, 4, 6, 15, 22, 21];
 const isthere = number.find(x => {
     if (x % 2 === 0) {
         return x;
