afd76002c6f58b3768984ddb4985d2a9125592b4
KazyMdNurulalam <kazymdnurulalam@gmail.com>
2021-10-26 20:38:47 +0600

    undefined reasions

---
+++ null-vs-undefined.js
@@ -0,0 +1,23 @@
+//  undefined easions list:
+
+
+// let age;
+
+// function add (num1, num2){
+//     console.log(num1 + num2);
+// }
+
+// function add (num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(10);
+
+// const person ={
+//     name: "sonu",
+//     age: 20,
+//     phone:0171234567,
+// }
+// console.log(person.address);
+
+// const age = [202, 21, 25, 27];
+// console.log(age[4]);
