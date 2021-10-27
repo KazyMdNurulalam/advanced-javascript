795ab815de12bbf94923d24468b3b1d011df0715
KazyMdNurulalam <kazymdnurulalam@gmail.com>
2021-10-27 15:50:32 +0600

    slice splice join

---
+++ Array-slice.js
@@ -0,0 +1,16 @@
+        //   slice
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const getNumbers = numbers.slice(2, 4);
+// console.log(getNumbers);
+// console.log(numbers);
+
+        //  splice 
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const getNumbers = numbers.splice(2, 4);
+// console.log(getNumbers);
+// console.log(numbers);
+
+        //  join
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const joinNumber = numbers.join(' ');
+// console.log(joinNumber);
